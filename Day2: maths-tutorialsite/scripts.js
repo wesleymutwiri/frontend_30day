9c82ad57b9d2b4087937b0c78aa4fcce7271ec30
wesleymutwiri <wesleymochiemo@gmail.com>
2019-08-20 18:42:17 +0300

    added javascript logic to make sure code works and generates new maths questions

--- Day2: maths-tutorialsite/scripts.js
+++ Day2: maths-tutorialsite/scripts.js
@@ -2,7 +2,7 @@ var playing = false;
 var score;
 var action;
 var timeremaining;
-
+var correctAnswer;
 document.getElementById("startreset").onclick = function(){
     if(playing == true){
         location.reload();
@@ -17,7 +17,32 @@ document.getElementById("startreset").onclick = function(){
         hide("gameover");
         document.getElementById("startreset").innerHTML = "Reset Game";
         startCountdown();
+        generateQA();
+    }
+}
 
+for(i=1; i<5; i++){
+    document.getElementById("box"+i).onclick = function () {
+        if (playing == true) {
+            if (this.innerHTML == correctAnswer) {
+                score++;
+                document.getElementById("scorevalue").innerHTML = score;
+                hide("wrong");
+                show("correct");
+                setTimeout(function () {
+                    hide("correct");
+                }, 1000);
+                generateQA();
+            }
+
+            else {
+                hide("correct");
+                show("wrong");
+                setTimeout(function () {
+                    hide("wrong");
+                }, 1000);
+            }
+        }
     }
 }
 
@@ -49,5 +74,21 @@ function show(id){
 }
 
 function generateQA(){
-
+    var x = 1+Math.round(9*Math.random());
+    var y = 1 + Math.round(9 * Math.random());
+    correctAnswer = x*y;
+    document.getElementById("question").innerHTML = x + "x" + y;
+    var correctPosition = 1 + Math.round(3 * Math.random()); 
+    document.getElementById("box" + correctPosition).innerHTML = correctAnswer;
+    var answers = [correctAnswer];
+    for(i=1; i<5; i++) {
+        if(i != correctPosition){
+            var wrongAnswer;
+            do{
+                wrongAnswer = (1 + Math.round(9 * Math.random())) * (1 + Math.round(9 * Math.random()));
+            }while(answers.indexOf(wrongAnswer)>-1)
+            document.getElementById("box"+i).innerHTML = wrongAnswer;
+            answers.push(wrongAnswer);
+        }
+    }
 }
