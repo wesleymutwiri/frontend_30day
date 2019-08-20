f505f2de7b2a5aa09322534f06ad5cac7bd10f7a
wesleymutwiri <wesleymochiemo@gmail.com>
2019-08-20 11:24:43 +0300

    prototype finally working correctly

--- Day1: web-fruit-ninja/scripts.js
+++ Day1: web-fruit-ninja/scripts.js
@@ -1,37 +1,37 @@
 var playing = false;
 var score;
 var trialsLeft;
-var fruits = ['apple', 'banana', 'grapes', 'cherries', 'peach', 'orange', 'pear', 'watermelon'];
 var step;
 var action;
+var fruits = ['apple', 'banana', 'grapes', 'cherries', 'peach', 'orange', 'pear', 'watermelon', 'mango'];
 
 $(function(){
-    $("#startreset").click(function(){
-        if(playing == true){
-            location.reload();
-        }
-        else {
-            playing = true;
-            score = 0;
-            $("#scorevalue").html(score);
-            $("#trialsleft").show();
-            trialsLeft = 3;
-            addHearts();
-            $("#gameover").hide();
-            $('#startreset').html("Reset Game");
-            startAction();
-        }
-
-    });
-    $("#fruit1").mouseover(function() {
-        score++;
+$("#startreset").click(function(){
+    if(playing == true){
+        location.reload();
+    }
+    else {
+        playing = true;
+        score = 0;
         $("#scorevalue").html(score);
-        // document.getElementById("slicesound").play();
-        $("#slicesound")[0].play();
-        clearInterval(action);
-        $("#fruit1").hide("explode", 500);
-        setTimeout(startAction, 500);
-    });
+        $("#trialsleft").show();
+        trialsLeft = 3;
+        addHearts();
+        $("#gameover").hide();
+        $('#startreset').html("Reset Game");
+        startAction();
+    }
+
+});
+$("#fruit1").mouseover(function() {
+    score++;
+    $("#scorevalue").html(score);
+    // document.getElementById("slicesound").play();
+    $("#slicesound")[0].play();
+    clearInterval(action);
+    $("#fruit1").hide("explode", 500);
+    setTimeout(startAction, 500);
+});
 
 
 function addHearts(){
@@ -44,27 +44,27 @@ function addHearts(){
 function startAction() {
     $("#fruit1").show();
     chooseFruit();
-    $("#fruit1").css({'left':Math.round(550*Math.random()), 'top': -50});
+    $("#fruit1").css({'left': Math.round(550*Math.random()), 'top' : -50});
     step = 1+ Math.round(5*Math.random());
     action = setInterval(function(){
         $("#fruit1").css('top', $("#fruit1").position().top + step);
-        if($("#fruit1").position.top > $("#fruitscontainer").height()){
-                if(trialsLeft > 1) {
-                    $("#fruit1").show();
-                    chooseFruit();
-                    $("#fruit1").css({ 'left': Math.round(550 * Math.random()), 'top': -50 });
-                    step = 1 + Math.round(5 * Math.random());
-                    trialsLeft-- ;
-                    addHearts();
-                }        
-                else {
-                    playing = false;
-                    $("#startreset").html("Start Game");
-                    $("#gameover").show();
-                    $("#gameover").html('<p>Game Over</p> <p> Your score is: ' + Score + '</p>');
-                    $("#trialsleft").hide();
-                    stopAction();
-                }
+
+        if($("#fruit1").position().top > $("#fruitscontainer").height()){
+            if(trialsLeft > 1) {
+                $("#fruit1").show();
+                chooseFruit();
+                $("#fruit1").css({'left': Math.round(550 * Math.random()), 'top': -50 });
+                step = 1 + Math.round(5 * Math.random());
+                trialsLeft --;
+                addHearts();
+            }else {
+                playing = false;
+                $("#startreset").html("Start Game");
+                $("#gameover").show();
+                $("#gameover").html('<p>Game Over</p> <p> Your score is: ' + score + '</p>');
+                $("#trialsleft").hide();
+                stopAction();
+            }
             } 
     }, 10);
 }
